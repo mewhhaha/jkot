77c84da05ea6cc3a54b11dc970004d0aeddbb5eb
Jacob Torrång <jacob.torrang@skf.com>
2022-02-26 15:47:04 +0100

    Do lookup on websocket level

--- app/routes/__shell/blog/$slug.edit.tsx
+++ app/routes/__shell/blog/$slug.edit.tsx
@@ -1,33 +1,24 @@
 import { useState, useEffect } from "react";
 import { LoaderFunction, useLoaderData } from "remix";
 import { requireAuthentication } from "~/services/auth.server";
-import { item } from "~/services/settings.server";
 
 export const loader: LoaderFunction = (args) =>
-  requireAuthentication(args, async ({ request, context, params }) => {
-    const article = await item(
-      request,
-      context,
-      `article/${params.slug}`
-    ).json();
-    if (article.id === undefined)
-      return new Response("Not found", { status: 404 });
-
-    return article.id;
+  requireAuthentication(args, async ({ params }) => {
+    return params.slug;
   });
 
 const useWebsocket = () => {
-  const id = useLoaderData<string>();
+  const slug = useLoaderData<string>();
   const [socket, setSocket] = useState<WebSocket>();
 
   useEffect(() => {
     const host = new URL(window.location.href).host;
-    const socket = new WebSocket(`wss://${host}/write/websocket/${id}`);
+    const socket = new WebSocket(`wss://${host}/write/websocket/${slug}`);
     setSocket(socket);
     return () => {
       socket.close();
     };
-  }, [id, setSocket]);
+  }, [slug, setSocket]);
 
   return socket;
 };
