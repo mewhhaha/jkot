4bb3ccff79db4e452a0c1591d254c5eae3f6d943
Jacob Torrång <jacob.torrang@skf.com>
2022-03-07 20:55:44 +0100

    Modal as div

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -45,7 +45,7 @@ export const Modal: ModalFC = ({ open, initialFocus, children, onClose }) => {
             &#8203;
           </span>
           <Transition.Child
-            as={Fragment}
+            as="div"
             enter="ease-out duration-300"
             enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             enterTo="opacity-100 translate-y-0 sm:scale-100"
