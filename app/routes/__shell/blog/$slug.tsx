fa342342b65ea18e4c96aba3c853f90369101793
Jacob Torrång <jacob.torrang@skf.com>
2022-03-12 21:50:33 +0100

    Fix viewable slug

--- app/routes/__shell/blog/$slug.tsx
+++ app/routes/__shell/blog/$slug.tsx
@@ -1,3 +1,43 @@
+import { LoaderFunction, useLoaderData } from "remix";
+import { ArticleFull } from "~/components/ArticleFull";
+import { articleKeys } from "~/services/article.server";
+import { CloudflareDataFunctionArgs, PublishedContent } from "~/types";
+
+type LoaderData = PublishedContent;
+
+export const loader: LoaderFunction = async ({
+  context,
+  params,
+}: CloudflareDataFunctionArgs): Promise<LoaderData> => {
+  const slug = params.slug;
+  if (slug === undefined) {
+    throw new Error("Invariant violated");
+  }
+
+  const content = await context.ARTICLE_KV.get<PublishedContent>(
+    articleKeys({ slug }).slugKey,
+    { type: "json" }
+  );
+
+  if (content === null) {
+    throw new Response("Not found", { status: 404 });
+  }
+
+  return content;
+};
+
 export default function Article() {
-  return <></>;
+  const content = useLoaderData<LoaderData>();
+  return (
+    <ArticleFull
+      title={content.title}
+      category={content.category}
+      description={content.description}
+      imageUrl={content.imageUrl}
+      imageAlt={content.imageAlt}
+      imageAuthor={content.imageAuthor}
+    >
+      {content.body}
+    </ArticleFull>
+  );
 }
