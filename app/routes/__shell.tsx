4d3d9eca58b4ca2842fcee269ed887def7340167
jacobtorrang <jacob.torrang@kits.se>
2022-03-24 16:29:36 +0100

    Fix padding

--- app/routes/__shell.tsx
+++ app/routes/__shell.tsx
@@ -103,7 +103,7 @@ export default function HeaderTemplate() {
   return (
     <div className="relative flex h-screen w-screen flex-col overflow-auto">
       <header className="relative flex w-full flex-col bg-white">
-        <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="relative mx-auto max-w-7xl py-1 sm:py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="flex justify-center text-base font-bold uppercase tracking-wide">
               <span className="bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent">
