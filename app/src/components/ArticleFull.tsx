2f2f17c1e71827a0b8121f2aa8e8db1b200b8fb9
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 17:32:28 +0200

    Highlight constantly

--- app/src/components/ArticleFull.tsx
+++ app/src/components/ArticleFull.tsx
@@ -28,7 +28,7 @@ export const ArticleFull: React.FC<ArticleFullProps> = ({
   useLayoutEffect(() => {
     if (ref.current === null || ref.current.parentNode === null) return;
     highlight(ref.current.parentNode);
-  }, [children, highlight]);
+  });
 
   return (
     <article className="h-full w-full overflow-hidden bg-white">
