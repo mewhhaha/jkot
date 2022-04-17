1a73fb61bc09cc7ac82f315c4baf881bef56e7e9
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 19:41:37 +0200

    Add reference to css

--- app/src/hooks/usePrism.tsx
+++ app/src/hooks/usePrism.tsx
@@ -1,7 +1,31 @@
-import { useRef } from "react";
+import { useInsertionEffect, useRef } from "react";
+import style from "prismjs/themes/prism-tomorrow.css";
 import Prism from "prismjs";
 
+let counter = 0;
+
+const css = (() => {
+  const el = document.createElement("link");
+  el.setAttribute("rel", "stylesheet");
+  el.setAttribute("href", style);
+  return el;
+})();
+
 export const usePrism = () => {
+  useInsertionEffect(() => {
+    if (counter === 0) {
+      document.head.appendChild(css);
+    }
+    counter++;
+
+    return () => {
+      counter--;
+      if (counter === 0) {
+        document.head.removeChild(css);
+      }
+    };
+  }, []);
+
   return useRef((node: ParentNode) => {
     Prism.highlightAllUnder(node);
   }).current;
