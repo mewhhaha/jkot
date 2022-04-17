e192a41150e61d51bd74ff8b29ce5a1f4dc5a5b2
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 16:07:25 +0200

    Add rows AND minrows

--- app/src/components/form/Textarea.tsx
+++ app/src/components/form/Textarea.tsx
@@ -31,6 +31,7 @@ export const Textarea: React.VFC<TextareaProps> = ({
           {...props}
           id={name}
           name={name}
+          rows={rows}
           minRows={rows}
           className={ocx(
             "mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-orange-500 focus:outline-none sm:text-sm"
