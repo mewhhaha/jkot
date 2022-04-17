573b37936f1d6c428685254e7a896425b5d84890
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 17:45:21 +0200

    Just remove things

--- app/src/hooks/usePrism.tsx
+++ app/src/hooks/usePrism.tsx
@@ -1,31 +1,7 @@
-import { useInsertionEffect, useRef } from "react";
-import style from "~/styles/prism.css";
+import { useRef } from "react";
 import Prism from "prismjs";
 
-let counter = 0;
-
-const css = (() => {
-  const el = document.createElement("link");
-  el.setAttribute("rel", "stylesheet");
-  el.setAttribute("href", style);
-  return el;
-})();
-
 export const usePrism = () => {
-  useInsertionEffect(() => {
-    if (counter === 0) {
-      document.head.appendChild(css);
-    }
-    counter++;
-
-    return () => {
-      counter--;
-      if (counter === 0) {
-        document.head.removeChild(css);
-      }
-    };
-  }, []);
-
   return useRef((node: ParentNode) => {
     Prism.highlightAllUnder(node);
   }).current;
