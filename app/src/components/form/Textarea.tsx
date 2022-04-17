a84f281a9e534af697083d8bdc3f2daf764d4254
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 20:10:42 +0200

    Try normal textarea

--- app/src/components/form/Textarea.tsx
+++ app/src/components/form/Textarea.tsx
@@ -27,12 +27,12 @@ export const Textarea: React.VFC<TextareaProps> = ({
         {label}
       </label>
       <div className="mt-1">
-        <TextareaAutosize
+        <textarea
           {...props}
           id={name}
           name={name}
           rows={rows}
-          minRows={rows}
+          // minRows={rows}
           className={ocx(
             "mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-orange-500 focus:outline-none sm:text-sm"
           )}
