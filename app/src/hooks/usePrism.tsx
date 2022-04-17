56ab988f47cdb351b4c2fe7f3438ad7e80724647
Jacob Torrång <jacob.torrang@skf.com>
2022-04-17 17:35:14 +0200

    Don't create new elements

--- app/src/hooks/usePrism.tsx
+++ app/src/hooks/usePrism.tsx
@@ -1,28 +1,26 @@
-import { useCallback, useInsertionEffect } from "react";
+import { useInsertionEffect } from "react";
 import style from "~/styles/prism.css";
 import Prism from "prismjs";
 
 let counter = 0;
 
+const css = document.createElement("link");
+css.setAttribute("rel", "stylesheet");
+css.setAttribute("href", style);
+
 export const usePrism = () => {
   useInsertionEffect(() => {
     if (counter === 0) {
-      const css = document.createElement("link");
-      css.setAttribute("rel", "stylesheet");
-      css.setAttribute("href", style);
       document.head.appendChild(css);
-
-      counter++;
-      return () => {
-        document.head.removeChild(css);
-        counter--;
-      };
-    } else {
-      counter++;
-      return () => {
-        counter--;
-      };
     }
+    counter++;
+
+    return () => {
+      counter--;
+      if (counter === 0) {
+        document.head.removeChild(css);
+      }
+    };
   }, []);
 
   return Prism.highlightAllUnder;
