55a9be0256744d860b7a361b6605c222fc843adc
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 02:12:53 +0200

    Update button

--- app/src/routes/admin/settings/stream.tsx
+++ app/src/routes/admin/settings/stream.tsx
@@ -44,19 +44,19 @@ const CategoryCombobox: React.FC<CategoryComboboxProps> = ({
   const [selected, setSelected] = useState(defaultValue);
 
   return (
-    <Listbox
-      as="div"
-      value={selected}
-      onChange={setSelected}
-      name={name}
-      className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500 sm:text-sm"
-    >
+    <Listbox as="div" value={selected} onChange={setSelected} name={name}>
       <Listbox.Label className="block text-sm font-medium text-gray-700">
         {label}
       </Listbox.Label>
       <div className="relative mt-1">
-        <Listbox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-          <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+        <Listbox.Button className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500 sm:text-sm">
+          <span className="block truncate">{selected}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <SelectorIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
         </Listbox.Button>
 
         <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
