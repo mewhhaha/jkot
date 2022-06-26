10b257e17b3f3f4cca30a50b6213145adb4f2f82
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 22:15:38 +0200

    Add transition blur

--- app/src/components/PageHeader.tsx
+++ app/src/components/PageHeader.tsx
@@ -1,3 +1,5 @@
+import { Transition } from "@headlessui/react";
+
 type PageHeaderProps = { about: string; title: string };
 
 export const PageHeader: React.FC<PageHeaderProps> = ({ about, title }) => {
@@ -6,9 +8,17 @@ export const PageHeader: React.FC<PageHeaderProps> = ({ about, title }) => {
       <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-12 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="flex justify-center text-base font-bold uppercase tracking-wide">
-            <span className="mt-1 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl sm:tracking-tight lg:text-6xl">
-              {title}
-            </span>
+            <Transition
+              show
+              appear
+              className="transition-[filter]"
+              enterFrom="blur-3xl"
+              enterTo="blur-0"
+            >
+              <span className="mt-1 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl sm:tracking-tight lg:text-6xl">
+                {title}
+              </span>
+            </Transition>
           </h2>
           <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">{about}</p>
         </div>
