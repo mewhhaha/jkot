5b8b271d788c604abef21b47123bb43ad869cc0e
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 14:22:20 +0200

    Update title

--- app/src/routes/__public.tsx
+++ app/src/routes/__public.tsx
@@ -84,16 +84,13 @@ export default function PublicTemplate() {
   return (
     <div className="relative flex h-screen w-screen flex-col overflow-auto">
       <header className="relative hidden w-full flex-col bg-white sm:flex">
-        <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-12 sm:px-6 lg:px-8">
           <div className="text-center">
-            <h2 className="flex justify-center text-base font-bold uppercase tracking-wide">
-              <span className="bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent">
-                {`${profile.username}`} Blog
-              </span>
-            </h2>
-            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-              jkot me; I fucked up!
-            </p>
+            <span className="bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent">
+              <h1 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                jkot me; I fucked up!
+              </h1>
+            </span>
             <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
               {profile.about}
             </p>
