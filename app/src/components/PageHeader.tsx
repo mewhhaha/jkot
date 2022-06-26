94b9e88b1a9af9abedfd3c949433c219c7ec82d6
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 20:54:53 +0200

    Fix header

--- app/src/components/PageHeader.tsx
+++ app/src/components/PageHeader.tsx
@@ -1,17 +1,11 @@
-import { CSSProperties, useRef } from "react";
-import Font from "~/fonts/water-galon.ttf";
-
 type PageHeaderProps = { about: string; title: string };
 
 export const PageHeader: React.FC<PageHeaderProps> = ({ about, title }) => {
-  const { current: fontStyle }= useRef<CSSProperties>({
-    fontFamily: 
-  })
   return (
     <header className="relative hidden w-full flex-col bg-white sm:flex">
       <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-12 sm:px-6 lg:px-8">
         <div className="text-center">
-          <h2 className="flex justify-center text-base font-bold uppercase tracking-wide font-['Cookie Monster']" >
+          <h2 className="font-['Cookie Monster'] flex justify-center text-base font-bold uppercase tracking-wide">
             <span className="mt-1 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl sm:tracking-tight lg:text-6xl">
               {title}
             </span>
