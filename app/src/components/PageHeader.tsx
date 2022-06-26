a093210e38815cb06fa4da82cbc324a3e6dc6b45
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 23:37:10 +0200

    Remove transition

--- app/src/components/PageHeader.tsx
+++ app/src/components/PageHeader.tsx
@@ -9,17 +9,9 @@ export const PageHeader: React.FC<PageHeaderProps> = ({ about, title }) => {
       <div className="relative mx-auto max-w-7xl py-16 px-4 sm:py-12 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="flex justify-center text-base font-bold uppercase tracking-wide">
-            <Transition
-              as={Fragment}
-              show
-              appear
-              enterFrom="blur-3xl"
-              enterTo="blur-0"
-            >
-              <span className="mt-1 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-4xl font-extrabold text-transparent transition-all duration-1000 sm:text-5xl sm:tracking-tight lg:text-6xl">
-                {title}
-              </span>
-            </Transition>
+            <span className="mt-1 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl sm:tracking-tight lg:text-6xl">
+              {title}
+            </span>
           </h2>
           <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">{about}</p>
         </div>
