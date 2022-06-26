3c8697fa742a81851891b851bdd49cd91e2f6a00
mewhhaha <kopatheonlyone@hotmail.com>
2022-06-26 18:00:24 +0200

    Speed up duration

--- app/src/routes/admin/blog/$slug.edit.tsx
+++ app/src/routes/admin/blog/$slug.edit.tsx
@@ -489,7 +489,7 @@ const Preview: React.FC<PreviewProps> = ({
       <button
         onClick={onUpdate}
         className={ocx(
-          "absolute top-0 right-0 flex w-full origin-top items-center justify-center bg-black bg-opacity-30 transition-transform duration-300 md:w-1/2",
+          "absolute top-0 right-0 flex w-full origin-top items-center justify-center bg-black bg-opacity-30 transition-transform duration-150 md:w-1/2",
           outdated ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"
         )}
       >
