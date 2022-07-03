cdba936610a02958e84356412c92ad6b501b304b
mewhhaha <kopatheonlyone@hotmail.com>
2022-07-03 02:16:16 +0200

    Reduce to 2px

--- app/src/components/form/Textedit.tsx
+++ app/src/components/form/Textedit.tsx
@@ -8,12 +8,12 @@ type TexteditProps = DetailedHTMLProps<
 
 export const Textedit: React.FC<TexteditProps> = ({ className, ...props }) => {
   return (
-    <div className="group relative border-b-4 border-black">
+    <div className="group relative border-b-2 border-black">
       <input
         className={ocx("w-full focus:outline-none", className)}
         {...props}
       />
-      <div className="absolute bottom-0 h-1 w-full translate-y-full scale-x-0 transform bg-orange-600 transition-transform group-focus-within:scale-x-100" />
+      <div className="absolute bottom-0 h-0.5 w-full translate-y-full scale-x-0 transform bg-orange-600 transition-transform group-focus-within:scale-x-100" />
     </div>
   );
 };
