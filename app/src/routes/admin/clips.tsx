9429fe9c24c19543f2e160a1f032a95a023be393
mewhhaha <kopatheonlyone@hotmail.com>
2022-07-09 21:44:27 +0200

    Fix type error

--- app/src/routes/admin/clips.tsx
+++ app/src/routes/admin/clips.tsx
@@ -19,7 +19,7 @@ export const loader: LoaderFunction = (args) =>
     args,
     async ({ request, context }): Promise<LoaderData> => {
       const settings = all(request, context, "video").json();
-      const all = all(request, context).json();
+      const a = all(request, context).json();
 
       const clips = await Promise.all(
         Object.values(settings)
@@ -46,7 +46,7 @@ export const loader: LoaderFunction = (args) =>
           })
       );
 
-      return { clips, extra: all };
+      return { clips, extra: a };
     }
   );
 
