91a8280e94abd4f452f7eee00ff59ab820a3b645
mewhhaha <kopatheonlyone@hotmail.com>
2022-07-16 16:53:58 +0200

    Improve websocket

--- app/src/hooks/useWebSocket.tsx
+++ app/src/hooks/useWebSocket.tsx
@@ -1,42 +1,69 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export type WebSocketStatus = "idle" | "error" | "closed" | "open";
+export type WebSocketStatus =
+  | "idle"
+  | "error"
+  | "closed"
+  | "open"
+  | "reconnecting";
 
-export const useWebSocket = (socketURL: string) => {
+export type UseWebSocketOptions = { reconnect: boolean; retries: number };
+
+export const useWebSocket = (
+  socketURL: string,
+  { reconnect, retries }: UseWebSocketOptions = { reconnect: true, retries: 10 }
+) => {
   const [status, setStatus] = useState<WebSocketStatus>("idle");
   const [socket, setSocket] = useState<WebSocket>();
 
+  const reconnects = useRef(0);
+
   useEffect(() => {
+    if (socket === undefined) {
+      setSocket(new WebSocket(socketURL));
+      return;
+    }
+
     try {
-      const socket = new WebSocket(socketURL);
       const handleError = () => {
         setStatus("error");
       };
 
-      const handleClose = () => {
-        setStatus("closed");
+      const handleClose = (ev: CloseEvent) => {
+        console.log(ev.code);
+        if (
+          ![1000, 1005].includes(ev.code) &&
+          reconnect &&
+          reconnects.current <= retries
+        ) {
+          reconnects.current += 1;
+          setStatus("reconnecting");
+          setSocket(new WebSocket(socketURL));
+        } else {
+          setStatus("closed");
+        }
       };
 
       const handleOpen = () => {
         setStatus("open");
+        reconnects.current = 0;
       };
 
       socket.addEventListener("error", handleError);
       socket.addEventListener("close", handleClose);
       socket.addEventListener("open", handleOpen);
 
-      setSocket(socket);
       return () => {
-        socket.close();
         socket.removeEventListener("error", handleError);
         socket.removeEventListener("close", handleClose);
         socket.removeEventListener("open", handleOpen);
+        socket.close();
       };
     } catch (error) {
       console.error(error);
       setStatus("error");
     }
-  }, [setSocket, socketURL]);
+  }, [reconnect, retries, setSocket, socket, socketURL]);
 
   return [socket, status] as const;
 };
